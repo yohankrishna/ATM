4140ecb392de81f548962e092b39e48494a70c1c
yohankrishna <135953973+yohankrishna@users.noreply.github.com>
2025-02-28 23:41:33 +0530

    Update Forgot.java

--- src/Forgot.java
+++ src/Forgot.java
@@ -188,8 +188,8 @@ public class Forgot extends javax.swing.JFrame {
         Random rand = new Random();
         randomcode=rand.nextInt(999999);
         String host = "smtp.gmail.com";
-        String user = "karmukilan.venkat2021@gmail.com";
-        String pass = "nduqrqsqpmyopyga";
+        String user = "abc@gmail.com"; // Use your email
+        String pass = "xxxxxxxxxxx"; // Use  your pass
         String to = txtEMAIL.getText();
         String subject = "Reseting code";
         String message = "your reset code is "+randomcode;
